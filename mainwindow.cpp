0f7c53fd9b6b2e14144a6fd212642ec95489dad9
DamienBidaud <dbdartian@gmail.com>
2015-04-03 17:09:42 +0200

    slot switche et traduire

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,37 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QObject::connect(this->ui->pushButton_2, SIGNAL(clicked()), this, SLOT(translate()));
+    QObject::connect(this->ui->pushButton, SIGNAL(clicked()), this, SLOT(change()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::translate(){
+    QString text = ui->textEdit->toPlainText();
+    if(text == "Je suis" || text == "I am"){
+        ui->textEdit_2->clear();
+        if(ui->label->text()=="Français"){
+            ui->textEdit_2->setText("I am");
+        }else{
+            ui->textEdit_2->setText("Je suis");
+        }
+    }else{
+        ui->textEdit_2->clear();
+        if(ui->label->text()=="Français"){
+            ui->textEdit_2->setText("No other trad available yet! Only one: Je suis ---> I am");
+        }else{
+            ui->textEdit_2->setText("Pas d'autre traduction disponible pour le moment! Uniquement une: Je suis ---> I am");
+        }
+    }
+}
+
+void MainWindow::change(){
+    QString label1 = ui->label->text();
+    ui->label->setText(ui->label_2->text());
+    ui->label_2->setText(label1);
 }
